be0880add0b3f08b9e8d4da2a61a357db9882e28
Markuspolos <aks17mark@ukr.net>
2022-01-21 16:29:35 +0200

    First commit

---
+++ homework21012022/homework21012022.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+void printSomeText();
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    printSomeText();
+}
+
+void printSomeText()
+{
+    std::cout << "Some text in function\n";
+}
+
